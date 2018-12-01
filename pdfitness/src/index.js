c1b5c692a7f82b4ca387c21f2dce45e3c0ff577e
thisisnull <lucas.poon7@gmail.com>
2018-12-01 16:40:39 -0500

    workout list and clients

--- pdfitness/src/index.js
+++ pdfitness/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Workout from './WorkoutTable';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Workout />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
